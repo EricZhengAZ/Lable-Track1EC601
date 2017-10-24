0feb3ec4c9e3227dc5d553ef3801639df9575513
Davide Lucchi <davidel@bu.edu>
2017-10-24 16:45:41 -0400

    merged code

--- MyApplication2/app/src/main/java/com/lcc/david/myapplication/MainActivity.java
+++ MyApplication2/app/src/main/java/com/lcc/david/myapplication/MainActivity.java
@@ -1,35 +1,49 @@
 package com.lcc.david.myapplication;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
-import android.hardware.Camera;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraManager;
-import android.support.v4.app.Fragment;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.icu.text.SimpleDateFormat;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.TextView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.facebook.login.LoginManager;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
+import static java.util.Calendar.DATE;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final int ACTIVITY_START_CAMERA_APP = 0;
+    private ImageView mPhotoCapturedImageView;
+    private String mImageFileLocation = "";
     private FirebaseAuth mAuth;
-    private Button cameraButton;
-    //private CameraManager mManager;
-    //private String[] mCameraIds;
-    private Camera mCamera;
-    private CameraPreview mPreview;
-
+    private Uri uriSavedImage;
+    private File image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPhotoCapturedImageView =(ImageView) findViewById(R.id.capturePhotoImageView);
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -41,65 +55,6 @@ public class MainActivity extends AppCompatActivity {
                 signOut();
             }
         });
-
-        Intent i = getIntent();
-        Bundle extras = i.getExtras();
-        String user = extras.getString(LogInActivity.USER);
-        ((TextView)findViewById(R.id.logged_user)).setText(user);
-
-
-        //mManager = (CameraManager)getSystemService(this.CAMERA_SERVICE);
-
-
-        cameraButton = (Button) findViewById(R.id.open_camera);
-        cameraButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v)
-            {
-                // open the camera
-                /*try {
-                    mCameraIds = mManager.getCameraIdList();
-                }
-                catch (CameraAccessException e)
-                {
-                    Toast t = Toast.makeText(v.getContext(),"Cannot open the camera",Toast.LENGTH_LONG);
-                    t.show();
-                }*/
-                // Create an instance of Camera
-                mCamera = getCameraInstance();
-
-                // Create our Preview view and set it as the content of our activity.
-                mPreview = new CameraPreview(v.getContext(), mCamera);
-                FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-                preview.addView(mPreview);
-
-            }
-        });
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        releaseCamera();              // release the camera immediately on pause event
-    }
-
-    private void releaseCamera(){
-        if (mCamera != null){
-            mCamera.release();        // release the camera for other applications
-            mCamera = null;
-        }
-    }
-
-
-    /** A safe way to get an instance of the Camera object. */
-    public static Camera getCameraInstance(){
-        Camera c = null;
-        try {
-            c = Camera.open(); // attempt to get a Camera instance
-        }
-        catch (Exception e){
-            // Camera is not available (in use or does not exist)
-        }
-        return c; // returns null if camera is unavailable
     }
 
     public void signOut() {
@@ -111,9 +66,83 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 
-    @Override
-    public void onBackPressed() {
-        // If the back button is pressed then SignOut
-        signOut();
+
+    public void takePhoto(View view){
+        Intent callCameraApplicationIntent = new Intent();
+        callCameraApplicationIntent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        //photoFile = null;
+
+            //photoFile = createImageFile();
+            //callCameraApplicationIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+            //startActivityForResult(callCameraApplicationIntent,ACTIVITY_START_CAMERA_APP);
+
+            File imagesFolder = new File(Environment.getExternalStorageDirectory(), "MyImages");
+            imagesFolder.mkdirs(); // <----
+            image = new File(imagesFolder, "image_001.jpg");
+            uriSavedImage = Uri.fromFile(image);
+            callCameraApplicationIntent.putExtra(MediaStore.EXTRA_OUTPUT, uriSavedImage);
+            startActivityForResult(callCameraApplicationIntent,ACTIVITY_START_CAMERA_APP);
+
+
     }
+
+    protected void onActivityResult (int requestCode, int resultCode, Intent data){
+        if (requestCode == ACTIVITY_START_CAMERA_APP && resultCode == RESULT_OK) {
+            //Toast.makeText(this, "Picture taken successfully", Toast.LENGTH_SHORT).show();
+            //Bundle extras = data.getExtras();
+            //Bitmap photoCaptureBitmap = (Bitmap) extras.get("data");
+            //mPhotoCapturedImageView.setImageBitmap(photoCaptureBitmap);
+
+
+            //Uri u = photoFile.toString();//data.getExtras().get("data");
+            //try {
+                //InputStream image_stream = getContentResolver().openInputStream(u);
+            //uriSavedImage.toString();
+            final File file = new File(Environment.getExternalStorageDirectory()
+                    .getAbsolutePath()+"/MyImages/image_001.jpg");
+
+            //uriSavedImage = Uri.fromFile(image);
+            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+            mPhotoCapturedImageView.setImageBitmap(bitmap);
+
+            System.out.println(Environment.getExternalStorageDirectory()
+                    .getAbsolutePath());
+           /* }
+            catch (FileNotFoundException e)
+            {
+                Toast.makeText(this,"File not found",Toast.LENGTH_SHORT).show();
+            }*/
+        }
+
+    }
+
+
+    File createImageFile() throws IOException{
+        String timeStamp = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        }
+        String imageFileName = "IMAGE_" + timeStamp + "_";
+        File storageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+        File image= File.createTempFile(imageFileName,".jpg",storageDirectory);
+        mImageFileLocation = image.getAbsolutePath();
+
+        return image;
+    }
+
+//         void setReducedImageSize(){
+//        int targetImageViewWidth =mPhotoCapturedImageView.getWidth();
+//        int targetImageViewHeight=mPhotoCapturedImageView.getHeight();
+//        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+//        bmOptions.inJustDecodeBounds = true;
+//        BitmapFactory.decodeFile(mImageFileLocation,bmOptions);
+//        int cameraImageWidth = bmOptions.outWidth;
+//       int cameraImageHeight= bmOptions.outHeight;
+//        int scaleFactor = Math.min(cameraImageWidth/targetImageViewWidth,cameraImageHeight/targetImageViewHeight);
+//        bmOptions.inSampleSize = scaleFactor;
+//        bmOptions.inJustDecodeBounds = false;
+//        Bitmap photoReducedSizeBitmap = BitmapFactory.decodeFile(mImageFileLocation,bmOptions);
+//        mPhotoCapturedImageView.setImageBitmap(photoReducedSizeBitmap);
+//    }
 }
